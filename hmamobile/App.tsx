f6a0534a668fbe6702d3fd8384e1ffb08239627a
Jerald <5033689@tvscredit.com>
2025-07-29 23:47:17 +0530

    get auth status

--- hmamobile/App.tsx
+++ hmamobile/App.tsx
@@ -8,8 +8,23 @@
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 
+import messaging from '@react-native-firebase/messaging';
+import { useEffect } from 'react';
+
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
+
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  requestUserPermission();
 
   return (
     <View style={styles.container}>
