0f35ef167403d89590cb6960bcf3189c5bb4ae72
Jerald <5033689@tvscredit.com>
2025-08-23 22:49:39 +0530

    add sb project

---
+++ hma-backend/src/main/java/com/hma/hma_backend/HmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hma.hma_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmaBackendApplication.class, args);
+	}
+
+}
---
+++ hma-backend/src/test/java/com/hma/hma_backend/HmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hma.hma_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
