cce5e74f95fe62ddbe0d2326a531e96f96bc61b8
Jerald <jerald.jayakumar@tvscredit.com>
2025-11-27 11:10:04 +0530

    update package name

--- hmamobile/android/app/src/main/java/com/attendokiosk/MainActivity.kt
+++ hmamobile/android/app/src/main/java/com/attendokiosk/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hmamobile
+package com.attendokiosk
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- hmamobile/android/app/src/main/java/com/attendokiosk/MainApplication.kt
+++ hmamobile/android/app/src/main/java/com/attendokiosk/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.hmamobile
+package com.attendokiosk
 
 import android.app.Application
 import com.facebook.react.PackageList
