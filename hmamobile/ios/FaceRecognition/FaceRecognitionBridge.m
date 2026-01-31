6aca80d519edd660a73119ac6c1071b9a1bb3ec7
Suriya <suriya@techxlns.com>
2026-01-31 22:18:59 +0530

    face recognition changes

--- hmamobile/ios/FaceRecognition/FaceRecognitionBridge.m
+++ hmamobile/ios/FaceRecognition/FaceRecognitionBridge.m
@@ -2,11 +2,10 @@
 
 @interface RCT_EXTERN_MODULE(FaceRecognition, NSObject)
 
-RCT_EXTERN_METHOD(
-  compare:(NSString *)first
-  with:(NSString *)second
-  resolver:(RCTPromiseResolveBlock)resolver
-  rejecter:(RCTPromiseRejectBlock)rejecter
-)
+RCT_EXTERN_METHOD(compare:(NSString *)first
+                  with:(NSString *)second
+                  rotation:(nonnull NSNumber *)rotation
+                  resolver:(RCTPromiseResolveBlock)resolver
+                  rejecter:(RCTPromiseRejectBlock)rejecter)
 
 @end
