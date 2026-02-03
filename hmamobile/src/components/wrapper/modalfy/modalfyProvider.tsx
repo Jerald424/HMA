67569d2fc30c9e8a154d23cbed7e814f620aba7e
Jerald MSI <ucajerald@gmail.com>
2026-02-03 21:46:38 +0530

    update modalfy

--- hmamobile/src/components/wrapper/modalfy/modalfyProvider.tsx
+++ hmamobile/src/components/wrapper/modalfy/modalfyProvider.tsx
@@ -1,8 +1,9 @@
 import { ReactNode } from 'react';
 import { createModalStack, ModalProvider } from 'react-native-modalfy';
+import { VerifyFaceModalFy } from 'src/components/layout/faceVerify';
 import Loader from './loader';
 
-const modalConfig = { Loader };
+const modalConfig = { Loader, FaceVerify: VerifyFaceModalFy };
 const defaultOptions = { backdropOpacity: 0.6 };
 
 const stack = createModalStack(modalConfig, defaultOptions);
